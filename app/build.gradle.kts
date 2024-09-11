cec04e0aac752398073a984e0c4452e536ca2590
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-09-11 20:48:26 +0300

    add lottie animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation (libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.swiperefreshlayout)
     implementation (libs.gson)
+    implementation (libs.lottie)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
