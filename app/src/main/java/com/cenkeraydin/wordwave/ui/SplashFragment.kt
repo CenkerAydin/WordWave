0b3468f8ec5b9285b84fe839798e139fa7514b00
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-09-12 10:04:22 +0300

    add splash fragment

---
+++ app/src/main/java/com/cenkeraydin/wordwave/ui/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.cenkeraydin.wordwave.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cenkeraydin.wordwave.R
+
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }, 2000)
+    }
+}
+
