e87ccd1519e29fbf97e64e575d6c6ce0cc1dc3bb
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-10-04 14:21:30 +0300

    add some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    alias(libs.plugins.com.google.dagger.hilt.android)
+    alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -51,6 +55,23 @@ dependencies {
     implementation(libs.androidx.swiperefreshlayout)
     implementation (libs.gson)
     implementation (libs.lottie)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.firebase.storage.ktx)
+    kapt(libs.hilt.android.compiler)
+
+    //Firebase
+    implementation(libs.firebase.auth)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.analytics)
+
+    implementation (libs.circleimageview)
+
+    //Glide
+    implementation (libs.glide)
+    kapt ("com.github.bumptech.glide:compiler:4.12.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
